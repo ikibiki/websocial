372b4936a272475dab164dbbdab3173b5021848a
Patrick Villar <patrickmarvin_villar@live.com>
2017-07-06 14:25:18 +0800

    Initial try-retry code
    - oauth2 client

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,29 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    function __construct() {
+        parent::__construct();
+
+        $this->load->library('Oauth2');
+    }
+
+    public function index() {
+
+        $this->load->view('home_page');
+    }
+
+    public function facebook() {
+        $this->oauth2->facebook();
+    }
+
+    public function instagram() {
+        $this->oauth2->instagram();
+    }
+    
+    public function googleplus() {
+        $this->oauth2->googleplus();
+    }
 }
