9e5174405be26fc3ffea25c9136cf42cb148ce42
Patrick Villar <patrickmarvin_villar@live.com>
2017-08-24 21:22:37 +0800

    Minor fix

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -61,20 +61,20 @@ class App extends CI_Controller {
             $lin = $this->input->post('LIN');
 
             if ($fb) {
-                $accesstoken = $this->SocialAccount->getSocialAccountByUser('FB', $user->ID)->AccessToken;
+                $accesstoken = $this->SocialAccount->getSocialAccountByUser('FB', $user->ID)[0]->AccessToken;
                 $resp = $this->facebookci->createPost($msg, $accesstoken);
                 $status .= 'Facebook successfully posted <a href="https://www.facebook.com/' . $resp['id'] . '">here</a>! ';
             }
 
             if ($tw) {
-                $twat = $this->SocialAccount->getSocialAccountByUser('TW', $user->ID);
+                $twat = $this->SocialAccount->getSocialAccountByUser('TW', $user->ID)[0];
                 $ats = explode(',', $twat->AccessToken);
                 $resp = $this->twitterci->tweet($msg, $ats[0], $ats[1]);
                 $status .= 'See your  <a href="https://twitter.com/' . $twat->SocialID . '/status/' . $resp->id . '">tweet</a>! ';
             }
 
             if ($lin) {
-                $accesstoken = $this->SocialAccount->getSocialAccountByUser('LIN', $user->ID)->AccessToken;
+                $accesstoken = $this->SocialAccount->getSocialAccountByUser('LIN', $user->ID)[0]->AccessToken;
                 $resp = $this->linkedinci->createPost($accesstoken, $msg);
                 $status .= 'Linked in status posted  <a href="' . $resp['updateUrl'] . '">here</a>! ';
             }
